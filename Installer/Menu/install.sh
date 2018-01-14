6c936584bae2e6533c3727a1e235afebeaab7f6b
stefan baumgartner <stefan-baumgartner@hotmail.com>
2018-01-14 05:10:30 +0100

    Updated

--- Installer/Menu/install.sh
+++ Installer/Menu/install.sh
@@ -1,42 +1,78 @@
 #!/bin/bash
 ############################################################
-# Developed by Store-Host 
+# Developed by Store-Host
 # Version 0.0.5.0
 # Web: https://www.store-host.com
-#	Store-Host is distributed in the hope that it will be useful,
+#       Store-Host is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #   GNU General Public License for more details.
 ############################################################
-# Installer menu
+# Installer Menu
 #
-INPUT=sh-installer_menu.sh.$$
-# trap and delete temp files
+url=http://bench.myvps.care/github/%0d/Installer
+htop=0
+lamp_php7=0
+goback=0
+INPUT=sh-installer.sh.$$
 trap "rm $INPUT; exit" SIGHUP SIGINT SIGTERM
-### display main menu ###
-dialog --clear  --backtitle "Store-Host" \
+dialog --clear --backtitle "Store-Host" \
 --title "[ M A I N - M E N U ]" \
 --menu "You can use the UP/DOWN arrow keys, the first \n\
 letter of the choice as a hot key, or the \n\
 number keys 1-9 to choose an option.\n\
 Choose the TASK" 0 0 8 \
-HTOP "Taskmanager" \
-Apache "Webserver." \
-LAMP-PHP5 "Apache - PHP 5 - MariaDB" \
-LAMP-PHP7 "Apache - PHP 7 - MariaDB" \
-PHP5 "Only PHP 5.6" \
+Htop "Taskmanager" \
+Lamp-PHP7 "PHP, Mysql, Apache" \
+Back "Back to the Mainmenu" \
 Exit "Exit to the shell" 2>"${INPUT}"
 menuitem=$(<"${INPUT}")
 # make decsion
 case $menuitem in
-        HTOP) do_htop=1;;
-        Apache2.4) do_apache=1;;
-		LAMP-PHP5) do_lamp5=1;;
-		LAMP-PHP7) do_lamp7=1;;
-		PHP5)	do_php5=1;;
-		Exit) echo "Bye"; break;;
+        htop) htop=1;;
+        Lamp-PHP7) lamp_php7=1;;
+		Back) goback=1;;
+
+        Exit) echo "Bye"; break;;
 esac
-# if temp files found, delete em
-#[ -f $OUTPUT ] && rm $OUTPUT
-[ -f $INPUT ] && rm $INPUT
-clear
+if [ $htop = 1 ] ; then
+
+			echo "Installing Htop.../n"
+			aptitude install htop -y  &>/dev/null
+			echo "htop is installed successfuly"
+fi
+if [ $lamp_php7 = 1 ] ; then
+
+			dialog --yesno "This can damage your system !! Do you want to continue?" 0 0
+		response=$?
+		case $response in
+		0) echo "Starte";;
+		1) echo exit;;
+		255) echo "[ESC] key pressed.";;
+		esac
+			if [ $response = 0 ] 
+			then
+				if cat /etc/issue.net | grep 9; then
+					mkdir /tmp/installer/
+					mkdir /tmp/installer/perl
+					cd /tmp/installer/perl
+					wget $url/Distribution/Debian/9/LAMP-PHP7.deb.pl
+					chmod -+x LAMP-PHP7.deb9.pl
+					perl LAMP-PHP7.deb9.pl
+					rm -f LAMP-PHP7.deb9.pl
+					clear
+				else
+					mkdir /tmp/installer/
+					mkdir /tmp/installer/perl
+					cd /tmp/installer/perl
+					wget $url/Distribution/Debian/8/LAMP-PHP7.deb.pl
+					chmod -+x LAMP-PHP7.deb.pl
+					perl LAMP-PHP7.deb.pl
+					rm -f LAMP-PHP7.deb.pl
+					clear
+				fi
+			fi
+fi
+if [ $goback = 1 ] ; then
+wget -O - $url/Menu/welcome.sh | bash
+fi
---
+++ Installer/Menu/interfaces.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+############################################################
+# Developed by Store-Host
+# Version 0.0.5.0
+# Web: https://www.store-host.com
+#       Store-Host is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+############################################################
+# Interaces-Installer Menu
+#
+url=http://bench.myvps.care/github/%0d/Installer
+forxlore=0
+imscp=0
+easywi=0
+goback=0
+INPUT=sh-installer.sh.$$
+trap "rm $INPUT; exit" SIGHUP SIGINT SIGTERM
+dialog --clear --backtitle "Store-Host" \
+--title "[ M A I N - M E N U ]" \
+--menu "You can use the UP/DOWN arrow keys, the first \n\
+letter of the choice as a hot key, or the \n\
+number keys 1-9 to choose an option.\n\
+Choose the TASK" 0 0 8 \
+Froxlor "Taskmanager" \
+IMSCP "PHP, Mysql, Apache" \
+Easy-Wi "Back to the Mainmenu" \
+Back "Back to the Mainmenu" \
+Exit "Exit to the shell" 2>"${INPUT}"
+menuitem=$(<"${INPUT}")
+# make decsion
+case $menuitem in
+        Froxlor) forxlore=1;;
+        IMSCP) imscp=1;;
+		Easy-Wi) easywi=1;;
+		Back) goback=1;;
+        Exit) echo "Bye"; break;;
+esac
+if [ $forxlore = 1 ] ; then
+	dialog --yesno "This can damage your system !! Do you want to continue?" 0 0
+		response=$?
+		case $response in
+		0) echo "Starting";;
+		1) echo exit;;
+		255) echo "[ESC] key pressed.";;
+		esac
+			if [ $response = 0 ] 
+			then
+				if cat /etc/issue.net | grep 9; then
+					echo Froxlore does not support debian 8 'Stretch'
+					exit 
+				else
+					touch /etc/apt/sources.list.d/froxlor.list
+					echo 'deb http://debian.froxlor.org jessie main' > /etc/apt/sources.list.d/froxlor.list &>/dev/null
+					apt-key adv --keyserver pool.sks-keyservers.net --recv-key FD88018B6F2D5390D051343FF6B4A8704F9E9BBC &>/dev/null
+					apt-get update && apt-get upgrade -y &>/dev/null
+					apt-get install php-curl &>/dev/null
+					apt-get install froxlor phpmyadmin -y &>/dev/null
+					sed -i 's/html/froxlor/g' /etc/apache2/sites-enabled/000-default.conf &>/dev/null
+					service apache2 restart
+					echo "Please enter your domain or IP address in the browser"
+					echo "Froxlor can now be set up"
+					clear
+				fi
+fi
+if [ $imscp = 1 ] ; then
+
+		dialog --yesno "This can damage your system !! Do you want to continue?" 0 0
+		response=$?
+		case $response in
+		0) echo "Starting";;
+		1) echo exit;;
+		255) echo "[ESC] key pressed.";;
+		esac
+			if [ $response = 0 ] 
+			then
+				if cat /etc/issue.net | grep 9; then
+					echo I-MSCP does not support debian 8 'Stretch'
+					exit 
+				else
+					apt-get update -y && apt-get upgrade -y &>/dev/null
+					apt-get install ca-certificates perl wget whiptail &>/dev/null
+					cd /usr/local/src
+					wget https://github.com/i-MSCP/imscp/archive/1.3.16.tar.gz &>/dev/null
+					tar -xzf 1.3.16.tar.gz &>/dev/null
+					cd imscp-1.3.16
+					perl imscp-autoinstall -d
+			fi
+fi
+if [ $easywi = 1 ] ; then
+	dialog --yesno "This can damage your system !! Do you want to continue?" 0 0
+		response=$?
+		case $response in
+		0) echo "Starting";;
+		1) echo exit;;
+		255) echo "[ESC] key pressed.";;
+		esac
+			if [ $response = 0 ] 
+			then
+				if cat /etc/issue.net | grep 9; then
+					echo Froxlore does not support debian 8 'Stretch'
+					exit 
+				else
+					echo "Easy web interface is prepared."
+					wget $url/Distribution/Debian/9/easy-wi_install.sh &>/dev/null
+					chmod -+x easy-wi_install.sh
+					./easy-wi_install.sh
+					echo "Easy-WI should be installed, should no errors have occurred"
+				fi
+fi
+if [ $goback = 1 ] ; then
+wget -O - $url/Menu/welcome.sh | bash
+fi
