982c7e5546142aae77cb4ac31d68d994ea70100c
stefan baumgartner <sb@store-host.com>
2018-02-03 17:37:24 +0100

    added mysql, mariadb, apache, nginx

--- overlay/installation.sh
+++ overlay/installation.sh
@@ -11,6 +11,10 @@
 lamp_php7=0
 docker=0
 goback=0
+nginx=0
+apache2=0
+mysql=0
+mariadb=0
 hostname=0
 INPUT=sh-installer.sh.$$
 trap "rm $INPUT; exit" SIGHUP SIGINT SIGTERM
@@ -20,6 +24,10 @@ dialog --clear --backtitle "[ M.O.C.I.S ]" \
 --menu "You can Install Apache, PHP, MySQL, and much more." 0 0 8 \
 Lamp-PHP7 "PHP, MySQL, Apache" \
 Docker "Docker engine (experimental)" \
+Nginx "Webserver" \
+Apache "Webserver" \
+MySQL "SQL Server" \
+MariaDB "SQL Server" \
 Back "Back to the Main Menu" \
 Exit "Exit to the shell" 2>"${INPUT}"
 menuitem=$(<"${INPUT}")
@@ -27,6 +35,10 @@ menuitem=$(<"${INPUT}")
 case $menuitem in
         Lamp-PHP7) lamp_php7=1;;
         Docker) docker=1;;
+        Nginx) nginx=1;;
+        Apache) apache2=1;;
+        MySQL) mysql=1;;
+        MariaDB) mariadb=1;;
 		    Back) goback=1;;
         Exit) echo "Bye"; break;;
 esac
@@ -58,4 +70,37 @@ if [ $docker = 1 ] ; then
   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
   apt-get update >/dev/null
   apt-get install docker-ce > /dev/null
+  bash /usr/share/mocis/overlay/welcome.sh
+fi
+if [ $nginx = 1 ] ; then
+  apt-get install nginx -y > /dev/null
+  nginxversion=$(nginx -v)
+  dialog --ascii-lines --backtitle "[ M.O.C.I.S ]" --title "Nginx Version" --msgbox "Check Nginx Version\n\n$nginxversion" 0 0
+  bash /usr/share/mocis/overlay/welcome.sh
+fi
+if [ $apache2 = 1 ] ; then
+  apt-get install apache2 -y > /dev/null
+  apache2version=$(apache2 -v)
+  dialog --ascii-lines --backtitle "[ M.O.C.I.S ]" --title "Apache Version" --msgbox "Check Apache2 Version\n\n$apache2version" 0 0
+  bash /usr/share/mocis/overlay/welcome.sh
+fi
+if [ $mysql = 1 ] ; then
+  clear
+  echo "Please wait installing in background\nThis can take some time"
+  apt-get install mysql-server -y > /dev/null
+  mysql_secure_installation
+  clear
+  mysqlversion=$(mysql --version)
+  dialog --ascii-lines --backtitle "[ M.O.C.I.S ]" --title "MySQL Version" --msgbox "Check MySQL Version\n\n$mysqlversion" 0 0
+  bash /usr/share/mocis/overlay/welcome.sh
+fi
+if [ $mariadb = 1 ] ; then
+  clear
+  echo "Please wait installing in background\nThis can take some time"
+  apt-get install mariadb-server -y > /dev/null
+  clear
+  mysql_secure_installation
+  mariadbversion=$(mariadb --version)
+  dialog --ascii-lines --backtitle "[ M.O.C.I.S ]" --title "MariaDB Version" --msgbox "Check MaraiDB Version\n\n$mariadbversion" 0 0
+  bash /usr/share/mocis/overlay/welcome.sh
 fi
